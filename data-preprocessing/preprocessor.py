f619d2ffb81d482ac72f62dc8fc5e0d2ad8eaa64
fossterer <shashank@linux.com>
2019-04-25 19:12:45 -0400

    Code organization:	Started organizing implementation into named stages of ML process. Also,  - renamed output directories to make more sense at each stage  - Added inital implementation for merging 2 different training sets into a common training set

--- data-preprocessing/preprocessor.py
+++ data-preprocessing/preprocessor.py
@@ -6,7 +6,7 @@ import csv
 #   Load characters.json file
 training_set_part1 = []
 
-with open("raw-data/characters.json") as input_file1:
+with open("input/characters.json") as input_file1:
         characters_data = json.load(input_file1)
 
 #   For each object in the array "characters", extract "characterName", sizeOf array "killed" and (if sizeOf array "killedBy" > 1, enter 1 else 0)
@@ -25,7 +25,7 @@ with open("raw-data/characters.json") as input_file1:
 #   Load episodes.json file
 training_set_part2 = []
 
-with open("raw-data/episodes.json") as input_file2:
+with open("input/episodes.json") as input_file2:
         episodes_data = json.load(input_file2)
 
 #   Create a disctionary of the form <name, episodeAppearanceCount>
@@ -51,13 +51,34 @@ with open("raw-data/episodes.json") as input_file2:
 
 # pprint(training_set_part2)
 
+# Extract only rows with data in all columns
+# i.e. for each character name, only if values exist in both training set parts, construct a new final training set that we would work on
+count = 0
+training_set = []
+
+for character_row in training_set_part1:
+        for episode_row in training_set_part2:
+                if character_row['character_name'] == episode_row['character_name']:
+                        training_row = OrderedDict()
+
+                        training_row['character_name'] = str(character_row['character_name'])
+                        training_row['episode_appearance_count'] = episode_row['episode_appearance_count']
+                        training_row['kill_count'] = character_row['kill_count']
+                        training_row['dead'] = character_row['dead']
+
+                        training_set.append(training_row)
+                        count += 1
+                        break
+        # break
+print count
+pprint(training_set)
 # Write 2 training sets into 2 csv files
-with open('training-data/characters.csv', 'w') as output_file1:
+with open('output/characters.csv', 'w') as output_file1:
         writer = csv.DictWriter(output_file1, training_set_part1[0].keys())
         writer.writeheader()
         writer.writerows(training_set_part1)
 
-with open('training-data/episodes.csv', 'w') as output_file2:
+with open('output/episodes.csv', 'w') as output_file2:
         writer = csv.DictWriter(output_file2, training_set_part2[0].keys())
         writer.writeheader()
         writer.writerows(training_set_part2)
